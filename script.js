3862656366fea8888563d63cd071393066695b1a
Rashedul Islam <santorasu09@gmail.com>
2023-06-05 22:53:44 +0600

    Update to the navber and first section

--- script.js
+++ script.js
@@ -8,19 +8,3 @@ menu.addEventListener('click', () => {
         target.classList.toggle('change');
     });
 });
-
-setInterval(() => {
-    i++
-    const icon1 = document.querySelector('.section-1-icons .change');
-
-    icon1.classList.remove('change');
-
-    //when last icon is reached, return to the first icon, else keep going.
-    if(i > icons.length){
-        icons[0].classList.add('change');
-        i = 1;
-    } else {
-        icon1.nextElementSibling.classList.add('change');  
-    } 
-
-}, 3000);
